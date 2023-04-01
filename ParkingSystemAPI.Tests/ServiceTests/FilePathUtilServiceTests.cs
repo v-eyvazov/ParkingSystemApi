aa3e0cbbaf46c33efeda8021c4033959a10b19ed
v-eyvazov <eyvazovvusal.1999@gmail.com>
2023-04-01 23:53:58 +0400

    Added FilePathUtilServiceTests

---
+++ ParkingSystemAPI.Tests/ServiceTests/FilePathUtilServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using ParkingSystemAPI.Services;
+using ParkingSystemAPI.Services.Impl;
+
+namespace ParkingSystemAPI.Tests.ServiceTests;
+
+public class FilePathUtilServiceTests
+{
+
+    private readonly IFilePathUtilService _filePathUtilService;
+    private readonly IConfiguration _configuration;
+    private readonly DirectoryDel _createDirectory;
+
+    public FilePathUtilServiceTests()
+    {
+
+        //Dependencies
+        _configuration = A.Fake<IConfiguration>();
+        _createDirectory = A.Fake<DirectoryDel>();
+
+        //SUT
+        _filePathUtilService = new FilePathUtilService(_configuration, _createDirectory);
+    }
+
+    [Theory]
+    [InlineData("Test")]
+    public void FilePathUtilService_GetFilePath_ReturnString(string filename)
+    {
+        //Arrange
+
+
+        //Act
+        string filePath = _filePathUtilService.GetFilePath(filename);
+
+        //Assert
+        filePath.Should().NotBeNullOrWhiteSpace();
+        filePath.Should().BeOfType<string>();
+        filePath.Should().Contain(".pdf");
+    }
+}
--- ParkingSystemAPI/Services/Impl/FilePathUtilService.cs
+++ ParkingSystemAPI/Services/Impl/FilePathUtilService.cs
@@ -1,23 +1,28 @@
 ﻿namespace ParkingSystemAPI.Services.Impl
 {
+
+    // for unit tests
+    public delegate DirectoryInfo DirectoryDel(string directory);
+
     public class FilePathUtilService : IFilePathUtilService
     {
         private readonly IConfiguration configuration;
+        private readonly DirectoryDel createDirectory;
 
-        public FilePathUtilService(IConfiguration configuration)
+        public FilePathUtilService(IConfiguration configuration, DirectoryDel? createDirectory = null)
         {
             this.configuration = configuration;
+            this.createDirectory = createDirectory ?? Directory.CreateDirectory;
         }
 
         public string GetFilePath(string filename)
         {
 
             string? directory = configuration["TicketDirectory"];
-            directory ??= "Null";
+            directory ??= "QRfiles";
 
             string filePath = Path.Combine(directory, filename + ".pdf");
-            Directory.CreateDirectory(directory);
-
+            createDirectory(directory);
             return filePath;
 
         }
